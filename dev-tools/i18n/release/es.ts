a5de0c2cee2199db0fb9addcc12d7f4beee2fdd7
sweetestmy26 <atlasaion2024@gmail.com>
2025-09-09 11:26:39 -0400

    Update es.ts
    45 bytes

--- dev-tools/i18n/release/es.ts
+++ dev-tools/i18n/release/es.ts
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="es" sourcelanguage="zh_CN">
+<TS version="2.1" language="es" sourcelanguage="SYS_CONTEXT">
 <context>
     <name>About</name>
     <message>
-        <location filename="../../../UmiOCR-data/qt_res/qml/TabPages/About/About.qml" line="44"/>
+        <location filename="" line=""/>
         <source>开源、免费的离线OCR软件</source>
         <translation>Software OCR off-line, gratuito de código abierto</translation>
     </message>
@@ -2452,7 +2452,7 @@
     <message>
         <location filename="../../../UmiOCR-data/qt_res/qml/Configs/UtilsConfigDicts.qml" line="89"/>
         <source>通知类型已更改</source>
-        <translation type="unfinished"></translation>
+        <translation type="PK"></translation>
     </message>
 </context>
 </TS>
